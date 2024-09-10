6b9b19c8f37cb27d12533561231c6c98688f2de3
Francisco Cáceres <francaceres98@gmail.com>
2024-09-10 12:56:44 -0300

    Added authorization requirements to match operations

--- src/modules/matches/matches.controller.ts
+++ src/modules/matches/matches.controller.ts
@@ -9,25 +9,37 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateMatchDTO, UpdateMatchDTO } from './dto';
 import { MatchesService } from './matches.service';
 import { FindManyOptionsDTO } from 'src/shared/dto';
+import { JwtAuthGuard } from '../auth/guards';
+import {
+  RoleBasedGroupAccessGuard,
+  VisibilityBasedGroupAccessGuard,
+} from 'src/shared/guards';
+import { GroupRoles } from 'src/shared/decorators';
+import { UserInGroupRole } from '@prisma/client';
 
 @Controller('/groups/:groupId/matches')
 export class MatchesController {
   constructor(private matchesService: MatchesService) {}
 
+  @UseGuards(JwtAuthGuard, RoleBasedGroupAccessGuard)
+  @GroupRoles([UserInGroupRole.ADMIN, UserInGroupRole.OWNER])
   @Post()
   createMatch(@Param('groupId') groupId: string, @Body() dto: CreateMatchDTO) {
     return this.matchesService.createMatch(groupId, dto);
   }
 
+  @UseGuards(VisibilityBasedGroupAccessGuard)
   @Get('/:matchId')
   getMatch(@Param('matchId') matchId: string) {
     return this.matchesService.getMatch(matchId);
   }
 
+  @UseGuards(VisibilityBasedGroupAccessGuard)
   @Get()
   getMatches(
     @Param('groupId') groupId: string,
@@ -36,6 +48,8 @@ export class MatchesController {
     return this.matchesService.getMatches(groupId, query);
   }
 
+  @UseGuards(JwtAuthGuard, RoleBasedGroupAccessGuard)
+  @GroupRoles([UserInGroupRole.ADMIN, UserInGroupRole.OWNER])
   @Patch('/:matchId')
   updateMatch(
     @Param('groupId') groupId: string,
@@ -45,6 +59,8 @@ export class MatchesController {
     return this.matchesService.updateMatch(groupId, matchId, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RoleBasedGroupAccessGuard)
+  @GroupRoles([UserInGroupRole.ADMIN, UserInGroupRole.OWNER])
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete('/:matchId')
   deleteMatch(@Param('matchId') matchId: string) {
