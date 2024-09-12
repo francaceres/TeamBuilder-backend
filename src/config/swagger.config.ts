66475a00667b472b25df8765e560f46fffdfc01e
Francisco Cáceres <francaceres98@gmail.com>
2024-09-12 12:00:44 -0300

    Add swagger to project

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('TeamBuilder API')
+    .setDescription(
+      'API used for the team building application called TeamBuilder. Developed by Francisco Cáceres',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  setupSwagger(app);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
